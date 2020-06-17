4fff6b60d704f3b9eed5b2d6f38b2b136a5f76aa
Imisioluwa Akande <imisioluwa.akande@andela.com>
2020-06-17 15:49:53 +0100

    feat: add web components

--- tfareapp/routes.py
+++ tfareapp/routes.py
@@ -1,11 +1,20 @@
 from tfareapp import app
+import json, plotly
 from flask import render_template
-from wrangling_scripts.wrangling import data_wrangling
+from wrangling_scripts.wrangling import return_figures
 
-data = data_wrangling()
-
-# print(data)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+
+    figures = return_figures()
+
+    # plot ids for the html id tag
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
+
+    # Convert the plotly figures to JSON for javascript in html template
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index.html',
+                           ids=ids,
+                           figuresJSON=figuresJSON)
